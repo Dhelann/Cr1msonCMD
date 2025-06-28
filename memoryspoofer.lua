5a0fe43b11170daf4b193cd8ffdc87dcbfb94e6d
Dhelan <gcool0780@gmail.com>
2025-06-28 20:10:38 +0800

    Create memoryspoofer.lua

---
+++ memoryspoofer.lua
@@ -0,0 +1,12 @@
+local gamemt=getrawmetatable(game)
+setreadonly(gamemt,false)
+local nc=gamemt.__namecall
+gamemt.__namecall=newcclosure(function(...)
+if getnamecallmethod()=="GetTotalMemoryUsageMb"then
+return math.random(395,405)
+end
+return nc(...)
+end)
+hookfunction(game.Stats.GetTotalMemoryUsageMb,function()
+return math.random(395,405)
+end)
