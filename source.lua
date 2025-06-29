534f6e5fd45468a2c0cedde2314113cb8f00ad71
Dhelan <gcool0780@gmail.com>
2025-06-29 13:08:50 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -256,5 +256,4 @@ end
 end
 Players.LocalPlayer.Chatted:Connect(function(msg)if msg:sub(1,1)==";"then runCommand(msg:sub(2))end end)
 pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhelann/Project-Cr1mson/refs/heads/main/loader.lua"))()end)
-pcall(function()loadstring(game:HttpGet("https://pastebin.com/raw/xpyxZ5fd"))()end)
 end
