fed2be593531e7fd2720dd0ca3943a68cafbbea7
Dhelan <gcool0780@gmail.com>
2025-06-29 13:07:19 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -257,5 +257,4 @@ end
 Players.LocalPlayer.Chatted:Connect(function(msg)if msg:sub(1,1)==";"then runCommand(msg:sub(2))end end)
 pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhelann/Project-Cr1mson/refs/heads/main/loader.lua"))()end)
 pcall(function()loadstring(game:HttpGet("https://pastebin.com/raw/xpyxZ5fd"))()end)
-pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhelann/Project-Cr1mson/refs/heads/main/spoofmemory.lua"))()end)
 end
